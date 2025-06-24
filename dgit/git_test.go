ac34b216656efa7fef14802484573c3fef54d2ca
ht <xx.abc@com>
2025-06-24 20:47:58 +0800

    add code

--- dgit/git_test.go
+++ dgit/git_test.go
@@ -2,10 +2,13 @@ package dgit
 
 import (
 	"context"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func Test_runCommand(t *testing.T) {
 	c := context.Background()
-	runCommand(c, "git", "rev-parse", "--is-inside-work-tree")
+	buf, err := runCommand(c, "git", "rev-parse", "--is-inside-work-tree")
+	require.NoError(t, err)
+	_ = buf
 }
