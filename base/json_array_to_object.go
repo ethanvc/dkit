efe9abeae2d39e09ba78075f259a6ab39469c547
ht <xx.abc@com>
2025-07-31 22:43:17 +0800

    add code

--- base/json_array_to_object.go
+++ base/json_array_to_object.go
@@ -1,6 +1,9 @@
 package base
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"github.com/ethanvc/dkit/simplepath"
+)
 
 func JsonArrayToObject(data []byte, config map[string]string) ([]byte, error) {
 	var dataObj any
@@ -8,14 +11,28 @@ func JsonArrayToObject(data []byte, config map[string]string) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+	dataObj, err = JsonAnyArrayToObject(dataObj, config)
+	if err != nil {
+		return nil, err
+	}
 	data, err = json.Marshal(dataObj)
 	return data, err
 }
 
-type jsonArrayToObjectConfig struct {
-	config map[string][]string
+func JsonAnyArrayToObject(data any, config map[string]string) (any, error) {
+	return jsonAnyArrayToObject(data, nil, config)
 }
 
-func newConfig(config map[string]string) jsonArrayToObjectConfig {
+func jsonAnyArrayToObject(data any, p simplepath.SimplePath, config map[string]string) (any, error) {
+	realData, ok := data.([]any)
+	if !ok {
+		return data, nil
+	}
+	return nil, nil
+}
 
+func findMatchedConfig(config map[string]string, currentP simplepath.SimplePath) (string, bool) {
+	for p, key := range config {
+
+	}
 }
